c9a36bb7e39d2a6817dbe121f3581162e458e30e
Luis Rujana <lmrujanass@gmail.com>
2020-12-01 22:20:24 -0500

    Added the sections of the README file to the generator module

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,30 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Table of Contents
+  1. [Description](#description "Description")
+  2. [Installation](#installation "Installation")
+  3. [Usage](#usage "Usage")
+  4. [License](#license "License")
+  5. [Contributing](#contributing "Contributing")
+  6. [Tests](#tests "Tests")
+
+  ##Description
+
+
+  ##Installation
+
+
+  ##Usage
+
+
+  ##License
+
+
+  ##Contributing
+
+
+  ##Tests
 `;
 }
 
